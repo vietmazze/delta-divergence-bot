0cd53c6f5eaca9e5f0ac5cfc481d973244b0d96a
vietmazze <dqnguyen2011@gmail.com>
2020-11-04 10:34:17 -0500

    testing3

--- binance.py
+++ binance.py
@@ -20,6 +20,7 @@ import time
 import os
 import threading
 import json
+import numpy as np
 
 logging.basicConfig(level=logging.INFO,
                     filename=os.path.basename(__file__) + '.log',
@@ -89,13 +90,60 @@ def process_aggTrade(data):
     except Exception as e:
         pass
 
+
+def process_totalVolume():
+    temp_trades = trades
+    buy_volume = 0
+    sell_volume = 0
+    try:
+        for trade in temp_trades:
+            if trade['side'] == "Sell":
+                sell_volume += trade['volume']
+            elif trade['side'] == "Buy":
+                buy_volume += trade['volume']
+        currDelta = buy_volume - sell_volume
+        delta.append(currDelta)
+    except Exception as e:
+        pass
+
+
+def calc_deltaDivergence():
+    neg_deltaDiv = [False] for __ in range(len(delta))
+    pos_deltaDiv = [False] for __ in range(len(delta))
+    # [positive,negative,negative]
+    # [negative,negative,positive]
+
+    # check for negative vol Bearish:
+    if max(delta) < 0:
+        # [-100,-200,-300]
+        for i in range(len(delta)):
+            if delta[i] > delta[i+1]:
+                neg_deltaDiv[i] = True
+            else:
+                neg_deltaDiv[i] = False
+    # check for positive vol Bullish:
+    elif min(delta) > 0:
+        # [300,200,100]
+        for i in range(0, len(delta)):
+            if delta[i] > delta[i+1]:
+                pos_deltaDiv[i] = True
+            else:
+                pos_deltaDiv[i] = False
+
+    # assign deltaDiv to the specific price spot or timestamp
+
+
 # Every 1min, take total of all trades and combine to get netvolume?
 # Every 1min, take the avg of price, or just get the last candle on the last min?
 # if positive vol1 > vol2 > vol3 and price1 > price2 > price3 = Bull
+# if negative vol1 > vol2 > vol3 and price1 < price2 < price3 = Bear
 
 
 worker_thread = threading.Thread(
     target=print_stream_data_buffer, args=(binance_websocket_api_manager,))
 worker_thread.start()
 
-time.sleep(5)
+"""
+Questions:
+1) Do we keep volume piling up, or volume for specific timeframe, and compare them
+"""

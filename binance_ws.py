6b63930b4273fa0dfed0ae25c6166cda3c422e74
vietmazze <dqnguyen2011@gmail.com>
2020-11-12 07:41:09 -0500

    deltas

--- binance_ws.py
+++ binance_ws.py
@@ -18,20 +18,28 @@ logging.basicConfig(level=logging.INFO, format=(
 class BinanceWebSocket:
     def __init__(self) -> None:
         self.prices = []
-        self.delta = []
         self.trades = []
+        self.vol_deltas = []
+        self.price_delta = []
+        self.buy_volume = 0
+        self.sell_volume = 0
         self.binance_websocket_manager = BinanceWebSocketApiManager(
             exchange="binance.com-futures")
-        self.channels = {}
-        self.markets = {}
+        self.channels = {'aggTrade', 'kline_1m'}
+        self.markets = {'btcusdt'}
         self.log = ColorPrint()
 
     def create_streams(self):
-        self.streams = self.binance_websocket_manager.create_stream(
-            self.channels, self.markets)
+        try:
+            self.binance_websocket_manager.create_stream(
+                self.channels, self.markets)
+
+        except Exception as e:
+            self.log.red(
+                f'Unable to create streams with {self.channels} and {self.markets}')
 
     def get_stream_data_buffer(self):
-        print("Initialize, wait 30secs for streams to come in")
+        self.log.yellow("Initialize, wait 5secs for streams to come in")
         time.sleep(5)
         while True:
             if self.binance_websocket_manager.is_manager_stopping():
@@ -43,10 +51,13 @@ class BinanceWebSocket:
                 time.sleep(0.01)
             else:
                 try:
-                    process_aggTrade(oldest_stream)
-
+                    if len(oldest_stream) > 4:
+                        self.process_aggTrade(oldest_stream)
+                    else:
+                        pass
                 except KeyError:
-                    print("error printing data")
+                    self.log.red(
+                        f'Unable to call process_aggTrade() from get_stream_data_buffer()')
                     # not able to process the data? write it back to the stream_buffer
                     self.binance_websocket_manager.add_to_stream_buffer(
                         oldest_stream)
@@ -65,15 +76,15 @@ class BinanceWebSocket:
                 elif data['is_market_maker'] == True:
                     trade['side'] = "Sell"
                 self.trades.append(trade)
-            if data['event_type'] == "kline":
-                if data['kline']['isClosed']:
+            elif data['event_type'] == "kline":
+                if data['kline']['is_closed']:
                     price['symbol'] = data['symbol']
                     price['interval'] = data['kline']['interval']
                     price['curr_time'] = data['event_time']
                     price['close_time'] = data['kline']['kline_close_time']
                     price['close_price'] = data['kline']['close_price']
                     self.prices.append(price)
-    #   trades = [{side:"buy",volume:2000},{..},{..}]
+
         except Exception as e:
             self.log.red(
                 f'Exception when processing buffer_stream in process_aggTrade(): \n {e}')
@@ -84,50 +95,50 @@ class BinanceWebSocket:
         try:
             for price in temp_prices:
                 price_tag.append(price['close_price'])
-            self.calc_priceDivergence(price_tag)
+            //self.calc_priceDivergence(price_tag)
         except Exception as e:
             pass
 
     def process_totalVolume(self) -> None:
         temp_trades = self.trades
-        buy_volume = 0
-        sell_volume = 0
         try:
             for trade in temp_trades:
                 if trade['side'] == "Sell":
-                    sell_volume += trade['volume']
+                    self.sell_volume += trade['volume']
                 elif trade['side'] == "Buy":
-                    buy_volume += trade['volume']
-            currDelta = buy_volume - sell_volume
-            self.delta.append(currDelta)
+                    self.buy_volume += trade['volume']
+            currDelta = self.buy_volume - self.sell_volume
+            self.vol_delta.append(float(currDelta))
+            self.trades = []
         except Exception as e:
             self.log.red(
                 f'Exception when processing in process_totalVolume():  \n {e}')
 
     def calc_volumeDivergence(self):
-        delta = self.delta
-        neg_deltaDiv = [False] for __ in range(len(delta))
-        pos_deltaDiv = [False] for __ in range(len(delta))
+        deltas = self.vol_deltas
+        bearish_delta = [[False] for __ in range(len(deltas))]
+        bullish_delta = [[False] for __ in range(len(deltas))]
         # [positive,negative,negative]
         # [negative,negative,positive]
 
         # check for negative vol Bearish:
         try:
-            if max(delta) < 0:
-                # [-100,-200,-300]
-                for i in range(len(delta)):
-                    if delta[i] > delta[i+1]:
-                        neg_deltaDiv[i] = True
-                    else:
-                        neg_deltaDiv[i] = False
-        # check for positive vol Bullish:
-            elif min(delta) > 0:
-                # [300,200,100]
-                for i in range(0, len(delta)):
-                    if delta[i] > delta[i+1]:
-                        pos_deltaDiv[i] = True
-                    else:
-                        pos_deltaDiv[i] = False
+            for i in range(len(deltas)-1):
+                # bearish
+                if deltas[i] > deltas[i+1]:
+                    # only accept positive entries:
+                    if deltas[i] > 0 and deltas[i+1] > 0:
+                        bearish_delta[i] = True
+                        bearish_delta[i+1] = True
+
+                elif deltas[i] < deltas[i+1]:
+                    if deltas[i] < 0 and deltas[i+1] < 0:
+                        bullish_delta[i] = True
+                        bullish_delta[i+1] = True
+                else:
+                    self.log.blue(
+                        f'Condition not met for deltas in calc_volumeDiv, {deltas[i]} vs {deltas[i+1]}')
+
         except Exception as e:
             self.log.red(
                 f'Exception when processing in calc_deltaDivergence():  \n {e}')

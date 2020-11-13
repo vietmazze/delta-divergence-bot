ce002dc5204419117e9ce1188f3ba546f2050c48
vietmazze <dqnguyen2011@gmail.com>
2020-11-13 11:31:42 -0500

    testing

--- binance_ws.py
+++ binance_ws.py
@@ -1,5 +1,4 @@
-from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
-from unicorn_fy.unicorn_fy import UnicornFy
+import collections
 import logging
 import time
 import os
@@ -7,6 +6,11 @@ import threading
 import json
 import schedule
 import numpy as np
+import asyncio
+
+from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
+from unicorn_fy.unicorn_fy import UnicornFy
+from timer import Timer
 from typing import Optional, Dict, Any, List
 from colorprint import ColorPrint
 from colorama import Fore, Back, Style, init
@@ -14,22 +18,29 @@ from colorama import Fore, Back, Style, init
 logging.basicConfig(level=logging.INFO, format=(
     Fore.BLUE + '[+] ' + Style.RESET_ALL + '%(message)s '))
 
+"""
+*self.prices = []  price from process_aggTrade(self, data)
+*self.agg_trades = []trades from each individual trade in process_aggTrade(self, data) 
+*self.total_volume = {}  Buy/Sell volume from each trade combined
+*self.total_deltas = []  Each net_volume calculated from total_volume with the current timestamp price
+
+"""
+
 
 class BinanceWebSocket:
     def __init__(self,) -> None:
         self.prices = []
-        self.trades = []
-        self.vol_deltas = []
-        self.price_deltas = []
+        self.agg_trades = []
+        self.total_volume = {'sell_volume': 0, 'buy_volume': 0, 'timestamp': 0}
+        self.total_deltas = []
         self.bearish_delta = []
         self.bullish_delta = []
-        self.buy_volume = 0
-        self.sell_volume = 0
         self.binance_websocket_manager = BinanceWebSocketApiManager(
             exchange="binance.com-futures")
         self.channels = {'aggTrade', 'kline_1m'}
         self.markets = {'btcusdt'}
         self.log = ColorPrint()
+        self.timer = Timer()
 
     def create_streams(self):
         try:
@@ -69,7 +80,8 @@ class BinanceWebSocket:
         price = {}
         try:
             if data['event_type'] == "aggTrade":
-
+                trade['symbol'] = data['symbol']
+                trade['curr_time'] = data['event_time']
                 trade['volume'] = float(
                     data['quantity']) * float(data['price'])
                 trade['side'] = 'None'
@@ -77,53 +89,75 @@ class BinanceWebSocket:
                     trade['side'] = "Buy"
                 elif data['is_market_maker'] == True:
                     trade['side'] = "Sell"
-                self.trades.append(trade)
+                self.agg_trades.append(trade)
+            # Continue to grab this grab this data, maybe once this is trigger we can THEN call delta_divergence()
             elif data['event_type'] == "kline":
                 if data['kline']['is_closed']:
                     price['symbol'] = data['symbol']
-                    price['interval'] = data['kline']['interval']
+                    price['timeframe'] = data['kline']['interval']
                     price['curr_time'] = data['event_time']
+                    price['start_time'] = data['kline']['kline_start_time']
                     price['close_time'] = data['kline']['kline_close_time']
-                    price['close_price'] = data['kline']['close_price']
+                    price['open'] = data['kline']['open_price']
+                    price['close'] = data['kline']['close_price']
+                    price['high'] = data['kline']['high_price']
+                    price['low'] = data['kline']['low_price']
                     self.prices.append(price)
+                    self.log.green(
+                        "Closed price event found, init process_totalVolume")
+                    self.process_totalVolume()
+                    self.process_combineData()
 
         except Exception as e:
             self.log.red(
                 f'Exception when processing buffer_stream in process_aggTrade(): \n {e}')
 
-    def process_totalPrice(self) -> None:
-        temp_prices = self.prices
-        price_tag = []
-        try:
-            for price in temp_prices:
-                price_tag.append(price['close_price'])
-            # self.calc_priceDivergence(price_tag)
-        except Exception as e:
-            pass
-
     def process_totalVolume(self) -> None:
-        temp_trades = self.trades
+        curr_agg_trades = self.agg_trades
+        self.timer.start()
         try:
-            for trade in temp_trades:
+            for trade in curr_agg_trades:
                 if trade['side'] == "Sell":
-                    self.sell_volume += trade['volume']
+                    #self.sell_volume += trade['volume']
+                    self.total_volume['sell_volume'] += float(trade['volume'])
                 elif trade['side'] == "Buy":
-                    self.buy_volume += trade['volume']
-            currDelta = self.buy_volume - self.sell_volume
-            self.vol_deltas.append(float(currDelta))
-            self.trades = []
+                    #self.buy_volume += trade['volume']
+                    self.total_volume['buy_volume'] += float(trade['volume'])
+                self.total_volume['timestamp'] = trade['curr_time']
+            self.agg_trades = []
         except Exception as e:
             self.log.red(
                 f'Exception when processing in process_totalVolume():  \n {e}')
+        self.timer.stop()
+
+    def process_combineData(self) -> None:
+        price = []
+        delta = 0
+        if len(self.total_volume) > 0:
+            try:
+                if len(self.total_volume) > 0 and len(self.prices) > 0:
+                    delta = float(
+                        self.total_volume['buy_volume'] - self.total_volume['sell_volume'])
+                    price = self.prices.pop()
+
+                total_delta = {
+                    "timestamp": float(price['curr_time']) if price['curr_time'] else 0,
+                    "delta": float(delta),
+                    "price": float(price['close']) if price['close'] else 0,
+                }
+                try:
+                    self.total_deltas.append(total_delta)
+                except Exception as e:
+                    self.log.red("Unable to append to total_deltas")
+                self.print_data()
+            except Exception as e:
+                self.log.red(f'Exception error in process_combineData() {e}')
 
     def calc_volumeDivergence(self):
         deltas = self.vol_deltas
         bearish_delta = [False for __ in range(len(deltas))]
         bullish_delta = [False for __ in range(len(deltas))]
-        # [positive,negative,negative]
-        # [negative,negative,positive]
 
-        # check for negative vol Bearish:
         try:
             for i in range(len(deltas)-1):
                 # bearish
@@ -146,26 +180,9 @@ class BinanceWebSocket:
             self.log.red(
                 f'Exception when processing in calc_deltaDivergence():  \n {e}')
 
-    # def calc_priceDivergence(self, price_tag):
-    #     #price_tag = []
-    #     price_decrease = [False for __ in range(len(price_tag))]
-    #     price_increase =[False for __ in range(len(price_tag))]
-    #     for i in range(len(price_tag)):
-    #         if price_tag[i] > price_tag[i+1]:
-    #             price_decrease[i] = True
-    #         else:
-    #             price_decrease[i] = False
-    #     for i in range(len(price_tag)):
-    #         if price_tag[i] < price_tag[i+1]:
-    #             price_increase[i] = True
-    #         else:
-    #             price_increase[i] = False
     def print_data(self):
         self.log.green(f"""
-
-                        Volume Delta : {self.vol_deltas} \n
-                        Bearish Delta: {self.bearish_delta} \n
-                        Bullish Delta: {self.bullish_delta}""")
+                        Delta: {self.total_deltas}""")
 
     def load_thread(self):
         # start a worker process to move the received stream_data from the stream_buffer to a print function
@@ -184,8 +201,8 @@ def main():
         binance.load_thread()
         time.sleep(5)
         schedule.every(1).minutes.do(binance.process_totalVolume)
-        schedule.every(1).minutes.do(binance.calc_volumeDivergence)
-        schedule.every(1).minutes.do(binance.print_data)
+        # schedule.every(1).minutes.do(binance.process_combineData)
+        # schedule.every(1).minutes.do(binance.print_data)
         while True:
             schedule.run_pending()
             time.sleep(1)
